b145cb3fb316ed82a659bdbe59af087b566e59d9
taryn yaeger <tsyaeger@gmail.com>
2018-08-20 00:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const logDriversByHometown = function (drivers, hometown) {
   })
 }
 
-const driversByRevenue = function (drivers, revenue) {
+const driversByRevenue = function (drivers) {
   
 }
 
