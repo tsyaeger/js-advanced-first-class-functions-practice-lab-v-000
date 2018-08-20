58ab153703f64861b4bb416e0923351b11e57d93
taryn yaeger <tsyaeger@gmail.com>
2018-08-20 01:09:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const driversByRevenue = function (drivers) {
 
 const driversByName = function (drivers) {
   return [...drivers].sort(function (a, b) {
-    return a.localeCompare(b);
+    return a.name.localeCompare(b.name);
   });
 };
 
