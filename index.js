5c4fe2129af34e63e69f5c0ed99bfb0f0a4f1770
taryn yaeger <tsyaeger@gmail.com>
2018-08-20 00:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const logDriverNames = function (drivers) {
   })
 }
 
-const logDriversByHometown = function (drivers) {
+const logDriversByHometown = function (drivers, hometown) {
   drivers.forEach(function(driver) {
     if (driver.hometown == hometown) {
       console.log(driver.name)
