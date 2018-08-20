f5d3ccf374aa3fcd055c8e9ab3c71c77e01f14df
taryn yaeger <tsyaeger@gmail.com>
2018-08-20 00:18:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,28 +8,8 @@ const logDriverNames = function (drivers) {
 
 const logDriversByHometown = function (drivers) {
   drivers.forEach(function(driver) {
-    console.log(driver.)
+    if (driver.hometown == hometown) {
+      console.log(driver.name)
+    }
   })
 }
-
-describe('logDriversByHometown()', function () {
-    let spy;
-
-    beforeEach(function () {
-      spy = sinon.spy(console, 'log');
-    });
-
-    afterEach(function () {
-      spy.restore();
-    });
-
-    it('logs the name of each driver whose hometown matches the passed-in argument', function () {
-      logDriversByHometown(drivers, 'Pittsburgh');
-
-      expect(spy.calledWithExactly('Bobby')).to.be.true;
-
-      expect(spy.calledWithExactly('Sammy')).to.be.true;
-
-      expect(spy.calledWithExactly('Annette')).to.not.be.true;
-    });
-  });
